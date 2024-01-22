6b4a0a4f603a4d4c0a35412a081ce2705574d598
peaga <pauloherbert@alu.ufc.br>
2024-01-22 11:44:42 -0300

    Add: some tests scenarios for api with cucumber

--- src/test/java/cucumber/steps/ProductStoreSteps.java
+++ src/test/java/cucumber/steps/ProductStoreSteps.java
@@ -1,24 +1,43 @@
 package cucumber.steps;
 
+import com.demobdd.controllers.ProductController;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.spring.CucumberContextConfiguration;
 
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 @CucumberContextConfiguration
 public class ProductStoreSteps {
+    private ProductController productController = new ProductController();
     @Given("have more than {int} products in store")
     public void haveMoreThanProductsInStore(int productQuantity) {
-
+        assertTrue(productController.findAll().size()>productQuantity,"have more than {int} products in store");
     }
-
-    @And("one of these products is a Notebook")
-    public void oneOfTheseProductsIsANotebook() {
-
+    @And("one of these products is a {string}")
+    public void oneOfTheseProductsIsA(String product) {
+        assertTrue(productController.findAll().contains(product),"one of these products is a {string}");
     }
-
     @Then("All products should be returned")
     public void allProductsShouldBeReturned() {
-
+        assertFalse(productController.findAll().isEmpty(),"All products should be returned");
     }
+    @Then("one product should be returned")
+    public void oneProductShouldBeReturned() {
+        String productName = "CellPhone";
+        assertEquals(productController.findByName(productName), productName, "one product should be returned");
+    }
+    @And("none of these products is a {string}")
+    public void noneOfTheseProductsIsA(String arg) {
+        assertFalse(productController.findAll().contains(arg),"none of these products is a {string}");
+    }
+    @Then("should be throw an error")
+    public void shouldBeThrowAnError() {
+        assertThrows(NoSuchElementException.class,()->productController.findByName("NonExistentProduct"),"should be throw a error");
+    }
+
+
 }
--- src/test/java/resources/features/Product.feature
+++ src/test/java/resources/features/Product.feature
@@ -1,5 +1,15 @@
 Feature: Products Store
   Scenario: Get all products in store
     Given have more than 3 products in store
-    And one of these products is a Notebook
+    And one of these products is a "Notebook"
     Then All products should be returned
+
+  Scenario: Get one product in store
+    Given have more than 0 products in store
+    And one of these products is a "CellPhone"
+    Then one product should be returned
+
+  Scenario: Get one product in store when product doesn't exist
+    Given have more than 0 products in store
+    And none of these products is a "NonExistentProduct"
+    Then should be throw an error
